205e4b5c9a452f67cd7833646b95cfbad6a7d077
Jason Huang <jasonhuang20035@gmail.com>
2020-11-28 20:37:04 -0500

    fix: formatting

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,13 @@ import { Flex, Heading, Spacer } from "@chakra-ui/react"
 
 const Header = ({ siteTitle }) => (
   <Flex as="header" w="100%" bgColor="blue.800">
-    <Flex m="0 auto" w="100%" maxW="960px" px={{ base: "2", sm: "4", md:"6" }} py="4">
+    <Flex
+      m="0 auto"
+      w="100%"
+      maxW="960px"
+      px={{ base: "2", sm: "4", md: "6" }}
+      py="4"
+    >
       <Heading size="xl" as="h1" m="0" color="white">
         {siteTitle}
       </Heading>
